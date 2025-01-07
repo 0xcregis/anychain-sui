4acade25be3ce07247918f227c1fe53cdb07bb44
aya015757881 <2581015450@qq.com>
2025-01-07 15:52:31 +0800

    fix: digest generation error

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,11 +1,12 @@
-use crate::{address::SuiAddress, format::SuiFormat, SuiPublicKey};
-use anychain_core::{PublicKey, Transaction, TransactionError, TransactionId};
+use crate::{address::SuiAddress, format::SuiFormat, SuiPrivateKey, SuiPublicKey};
+use anychain_core::{Address, Transaction, TransactionError, TransactionId};
 
-use fastcrypto::hash::{Blake2b256, HashFunction};
+use fastcrypto::{ed25519::{Ed25519KeyPair, Ed25519PrivateKey, Ed25519Signature}, hash::{Blake2b256, HashFunction}, traits::KeyPair};
 use base64::engine::{Engine, general_purpose::STANDARD};
+use shared_crypto::intent::{Intent, IntentMessage};
 use std::fmt::Display;
 use sui_types::{
-    base_types::ObjectID, object::Object, transaction::TransactionData
+    base_types::ObjectID, crypto::{Signer, ToFromBytes}, object::Object, transaction::TransactionData
 };
 use serde_json::{json, Value};
 use core::str::FromStr;
@@ -37,7 +38,7 @@ impl Transaction for SuiTransaction {
         Ok(SuiTransaction { params: params.clone(), signature: None})
     }
 
-    fn sign(&mut self, rs: Vec<u8>, recid: u8) -> Result<Vec<u8>, TransactionError> {
+    fn sign(&mut self, rs: Vec<u8>, _: u8) -> Result<Vec<u8>, TransactionError> {
         if rs.len() != 64 {
             return Err(TransactionError::Message(format!(
                 "Invalid signature length {}",
@@ -67,29 +68,30 @@ impl Transaction for SuiTransaction {
             self.params.gas_price,
         );
 
-        let raw_tx = bcs::to_bytes(&data)
-            .map_err(|e| TransactionError::Message(e.to_string()))?;
-
-        // let msg = IntentMessage::new(Intent::sui_transaction(), data);
-        // let raw_tx = bcs::to_bytes(&msg)
-        //     .map_err(|e| TransactionError::Message(e.to_string()))?;
-
         match &self.signature {
             Some(sig) => {
                 let flag = vec![0u8]; // 0 indicates ed25519 scheme
                 let pk = self.params.public_key.clone();
                 let sig = [flag, sig.clone(), pk].concat();
 
+                let raw_tx = bcs::to_bytes(&data)
+                    .map_err(|e| TransactionError::Message(e.to_string()))?;
+
                 let raw_tx = STANDARD.encode(raw_tx);
                 let sig = STANDARD.encode(sig);
 
                 Ok(json!([raw_tx, sig]).to_string().as_bytes().to_vec())
             }
-            None => Ok(raw_tx)
+            None => {
+                let msg = IntentMessage::new(Intent::sui_transaction(), data);
+                let msg = bcs::to_bytes(&msg)
+                    .map_err(|e| TransactionError::Message(e.to_string()))?;
+                Ok(msg)
+            }
         }
     }
 
-    fn from_bytes(stream: &[u8]) -> Result<Self, TransactionError> {
+    fn from_bytes(_stream: &[u8]) -> Result<Self, TransactionError> {
         todo!()
     }
 
@@ -103,7 +105,7 @@ impl Transaction for SuiTransaction {
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
-pub struct SuiTransactionId([u8; 32]);
+pub struct SuiTransactionId(pub [u8; 32]);
 
 impl TransactionId for SuiTransactionId {}
 
@@ -120,30 +122,19 @@ fn test_object_id() {
 }
 
 #[test]
-fn test() {
-    let flag = vec![0u8]; // 0 indicates ed25519 scheme
-    let format = &SuiFormat::Hex;
+fn test_tx() {
+    let sk_from = "suiprivkey1qzjx78cfcqww8prl6cw569rgz3v095a5qc3kae93374nhn23r9w0xqh7628";
+    let sk_to = "suiprivkey1qz4geqyqpa83waxmnf2vr80qemktms0gzthy5r07j4naaettnvwpkf6swws";
 
-    let from_pk = [177, 212, 63, 234, 239, 192, 246, 9, 235, 235, 146, 97, 147, 205, 50, 42, 81, 190, 242, 142, 212, 247, 57, 84, 153, 89, 7, 172, 131, 208, 76, 118].to_vec();
-    let to_pk = [152, 185, 21, 134, 56, 146, 96, 192, 175, 224, 37, 105, 223, 59, 74, 27, 96, 178, 129, 249, 151, 151, 106, 53, 123, 20, 221, 212, 197, 205, 42, 86].to_vec();
-    
-    let from = [flag.clone(), from_pk.clone()].concat();
-    let to = [flag, to_pk].concat();
-    
-    let from = STANDARD.encode(from);
-    let to = STANDARD.encode(to);
-
-    let from = SuiPublicKey::from_str(&from).unwrap();
-    let to = SuiPublicKey::from_str(&to).unwrap();
-
-    let from = from.to_address(format).unwrap();
-    let to = to.to_address(format).unwrap();
+    let from = sk_to_addr(sk_from);
+    let to = sk_to_addr(sk_to);
 
     println!("from: {}\nto: {}", from, to);
 
     let amount = 1000000000;
     let gas_budget = 300000;
     let gas_price = 750;
+    let public_key = sk_to_pk(sk_from);
     
     let tx = SuiTransactionParameters {
         from,
@@ -151,27 +142,49 @@ fn test() {
         amount,
         gas_budget,
         gas_price,
-        public_key: from_pk,
+        public_key,
     };
 
     let mut tx = SuiTransaction::new(&tx).unwrap();
-    let txid = tx.to_transaction_id().unwrap();
+    
+    let txid = tx.to_transaction_id().unwrap().0.to_vec();
+    let sig = sk_sign(sk_from, &txid);
 
-    println!("txid: {}", txid);
-
-    let r = "c78ca2036917b70b99ccfe2e30e5152693f8045f3a0d031cddd98f4e69563974";
-    let s = "e2b9f4d673b8b5ff37b62a6703a96ad6acb3c57f6cbc6a601056990e2c0c0105";
-    let recid = 0;
-
-    let r = hex::decode(r).unwrap();
-    let s = hex::decode(s).unwrap();
-
-    let rs = [r, s].concat();
-
-    let tx = tx.sign(rs, recid).unwrap();
+    let tx = tx.sign(sig, 0).unwrap();
     let tx = String::from_utf8(tx).unwrap();
 
     let tx = serde_json::from_str::<Value>(&tx).unwrap();
 
     println!("{}", tx);
+}
+
+use bech32::FromBase32;
+
+fn sk_to_addr(sk: &str) -> SuiAddress {
+    let (_, data, _) = bech32::decode(sk).unwrap();
+    let data = Vec::from_base32(&data).unwrap();
+    let sk = Ed25519PrivateKey::from_bytes(&data[1..]).unwrap();
+    let sk = SuiPrivateKey::Ed25519(sk);
+    let addr = SuiAddress::from_secret_key(&sk, &SuiFormat::Hex).unwrap();
+    addr
+}
+
+fn sk_to_pk(sk: &str) -> Vec<u8> {
+    let (_, data, _) = bech32::decode(sk).unwrap();
+    let data = Vec::from_base32(&data).unwrap();
+    let sk = Ed25519PrivateKey::from_bytes(&data[1..]).unwrap();
+    let keypair = Ed25519KeyPair::from(sk);
+    let pk = keypair.public();
+    let pk = pk.as_bytes();
+    pk.to_vec()
+}
+
+fn sk_sign(sk: &str, msg: &[u8]) -> Vec<u8> {
+    let (_, data, _) = bech32::decode(sk).unwrap();
+    let data = Vec::from_base32(&data).unwrap();
+    let sk = Ed25519PrivateKey::from_bytes(&data[1..]).unwrap();
+    let keypair = Ed25519KeyPair::from(sk);
+    let sig: Ed25519Signature = keypair.sign(msg);
+    let sig = sig.sig.to_bytes().to_vec();
+    sig
 }
